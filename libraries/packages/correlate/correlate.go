7b5a03d3f1d50d1bf49cddb99c09c8fbd8a1dae1
Javar Harris <javarharris@gmail.com>
2026-01-21 20:15:03 +0000

    +semver:minor

--- libraries/packages/correlate/correlate.go
+++ libraries/packages/correlate/correlate.go
@@ -19,7 +19,7 @@ const (
 
 type TraceId string          // trace id to navigate the request
 type Status string           // status of the request
-type Description *string     // description in the request
+type Description string     // description in the request
 type HandlerName interface{} //  handler name of the request
 type RequestEventTs string   // timestamp of the request's event
 type ClientRequestHeaders map[string]any
@@ -45,28 +45,28 @@ func NewCorrelateRequest(request *http.Request, fnName interface{}) CorrelateSer
 
 // CorrelateService - Info, Except, Success, Fatal
 type CorrelateService interface {
-	Info(st *Status, desc *Description)
-	Except(st *Status, desc *Description) error
-	Success(st *Status, desc *Description)
-	Fatal(st *Status, desc *Description) error
+	Info(st Status, desc Description)
+	Except(st Status, desc Description) error
+	Success(st Status, desc Description)
+	Fatal(st Status, desc Description) error
 }
 
-func (c *Correlate) Info(st *Status, desc *Description) {
-	info := fmt.Sprintf(CORR_DETAILS, INFO, c.TraceId, &st, &desc, c.RemoteAddr, c.URL.Path, fn(c.HandlerName), c.ClientRequestHeaders)
-	
+func (c *Correlate) Info(st Status, desc Description) {
+	info := fmt.Sprintf(CORR_DETAILS, INFO, c.TraceId, st, desc, c.RemoteAddr, c.URL.Path, fn(c.HandlerName), c.ClientRequestHeaders)
+
 	fmt.Println(info)
 
 }
-func (c *Correlate) Except(st *Status, desc *Description) error {
-	exception := fmt.Sprintf(CORR_DETAILS, EXCEPT, c.TraceId, &st, &desc, c.RemoteAddr, c.URL.Path, fn(c.HandlerName), c.ClientRequestHeaders)
+func (c *Correlate) Except(st Status, desc Description) error {
+	exception := fmt.Sprintf(CORR_DETAILS, EXCEPT, c.TraceId, st, desc, c.RemoteAddr, c.URL.Path, fn(c.HandlerName), c.ClientRequestHeaders)
 
 	return fmt.Errorf("%v", exception)
 }
-func (c *Correlate) Success(st *Status, desc *Description) {
+func (c *Correlate) Success(st Status, desc Description) {
 
 }
-func (c *Correlate) Fatal(st *Status, desc *Description) error {
-	fatal := fmt.Sprintf(CORR_DETAILS, FATAL, c.TraceId, &st, &desc, c.RemoteAddr, c.URL.Path, fn(c.HandlerName), c.ClientRequestHeaders)
+func (c *Correlate) Fatal(st Status, desc Description) error {
+	fatal := fmt.Sprintf(CORR_DETAILS, FATAL, c.TraceId, st, desc, c.RemoteAddr, c.URL.Path, fn(c.HandlerName), c.ClientRequestHeaders)
 
 	return fmt.Errorf("%s", fatal)
 }
